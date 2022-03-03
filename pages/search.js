7feb0d8ead4cbc18e3e1e815820ebc32d5c5819f
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-03 22:31:43 +0100

    useState imported

--- pages/search.js
+++ pages/search.js
@@ -1,38 +1,59 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import Image from 'next/image'
-import { Flex, Box, Text, Icon } from '@chakra-ui/react';
-import { BsFilter } from 'react-icons/bs';
-import SearchFilters from '../components/SearchFilters';
-import Property from '../components/Property';
-import noresult from '../assets/images/noresult.svg'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { Flex, Box, Text, Icon } from "@chakra-ui/react";
+import { BsFilter } from "react-icons/bs";
+import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import noresult from "../assets/images/noresult.svg";
 
+const Search = () => {
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
 
+  return (
+    <Box>
+      <Flex
+        onClick={() => setSearchFilters((prev) => !prev)}
+        cursor="pointer"
+        bg="gray.100"
+        borderBottom="1px"
+        p="2"
+        borderColor="gray.200"
+        fontWeight="Black"
+        fontSize="lg"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>Search Property By Filters</Text>
+        <Icon paddingLeft="2" w="7" as={BsFilter} />
+      </Flex>
 
-const Search =()=>{
-    const [searchFilters, setSearchFilters]=useState(false);
-    const router=useRouter();
-
-    return(
-        <Box>
-        <Flex onClick={()=>setSearchFilters((prev)=> !prev)} cursor="pointer" bg="gray.100" borderBottom="1px" p="2" borderColor="gray.200" fontWeight="Black" fontSize="lg" justifyContent="center" alignItems="center" >
-            <Text>Search Property By Filters</Text> 
-            <Icon paddingLeft='2' w='7' as={BsFilter} />
-        </Flex>
-
-        {searchFilters && <SearchFilters />}
-         <Text fontSize="2xl" p="4" fontWeight="bold">Properties {router.query.purpose} </Text>
-         <Flex flexWrap='wrap'>
-        {[].map((property) => <Property property={property} key={property.id} />)}
+      {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" p="4" fontWeight="bold">
+        Properties {router.query.purpose}{" "}
+      </Text>
+      <Flex flexWrap="wrap">
+        {[].map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
       </Flex>
       {properties.length === 0 && (
-        <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5'>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDir="column"
+          marginTop="5"
+          marginBottom="5"
+        >
           <Image src={noresult} />
-          <Text fontSize='xl' marginTop='3'>No Result Found.</Text>
+          <Text fontSize="xl" marginTop="3">
+            No Result Found.
+          </Text>
         </Flex>
       )}
-        </Box>
-    )
-}
+    </Box>
+  );
+};
 
 export default Search;
