2adf4f338ec346a4a2660ba04821170538739f32
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-03 03:13:38 +0100

    added Navbar component and Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import {ChakraProvider} from '@chakra-ui/react'
+import Head from 'next/head';
+import Nprogress from 'nprogress';
+
+import Layout from '../components/Layout';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+  <Head>
+
+  </Head>
+  <ChakraProvider>
+    <Layout title="Real Estate">
+      <Component {...pageProps} />    
+    </Layout>
+  </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
