88029de96268b5466891cba7ba966bb09c0ed93a
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-03 22:20:29 +0100

    added footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/layout';
+
+const Footer=()=>{
+    const year= new Date().getFullYear()
+    return (
+         <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+         © {year} BUY & RENT, Inc.
+          </Box>
+    
+    )
+}
+export default Footer;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import {Box} from '@chakra-ui/react';
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout =({children, title})=>{
    return(
@@ -15,7 +16,7 @@ const Layout =({children, title})=>{
 
         <main>{children}</main>
 
-        <footer>Footer</footer>
+        <footer><Footer/></footer>
 
     </Box>
     </>
